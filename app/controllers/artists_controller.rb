4010cb53f55e1f5a3638961366db28531f42a7cf
Miles Marillo <mjm03@hampshire.edu>
2020-01-02 13:45:22 -0500

    up and runnin

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,4 +4,8 @@ def index
 @artists = Artist.all
 end
 
+def show
+    @artist = Artist.find(params[:id])
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 resources :artists
+resources :albums
+resources :songs
+resources :lyrics
+
 end
