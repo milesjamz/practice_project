018e7bb50f8196a24e5c1e8d97a52572e63586d0
Miles Marillo <mjm03@hampshire.edu>
2020-01-02 15:36:16 -0500

    more actions

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,4 +8,14 @@ def show
     @artist = Artist.find(params[:id])
 end
 
+def create
+    @artist = Artist.create(artist_params)
+end
+
+private
+
+def artist_params
+    params.require(:artist).permit(:name, :biography)
+end
+
 end
