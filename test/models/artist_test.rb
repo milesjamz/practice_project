4ed5dd24139c74606dadb166fd935b9a230008cf
Miles Marillo <mjm03@hampshire.edu>
2020-01-13 09:46:20 -0500

    testing

--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ArtistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without biography and name" do
+    artist = Artist.new
+    assert artist.save
+  end
 end
